3992398b00b095876f48d886ddb67dc2a130ab07
Suraj Pathak <officialsuraj136@gmail.com>
2023-10-19 20:36:48 +0530

    added index.js

---
+++ index.js
@@ -0,0 +1,88 @@
+ // [1,2,3,4]
+// 
+// Filter, forEach, Map, Reduce 
+
+
+// let res  = arr.filter((ele)=>{
+//   return ele % 2 == 0
+// })
+
+// console.log(res)
+
+
+//  let res = arr.forEach((ele)=>{
+//    // console.log(ele % 2 == 0)
+
+//    ele % 2 == 0 ? console.log(ele) : ""
+// })
+
+// console.log(res)
+
+
+// let arr = ["suraj","js",'suraj']
+// function myFilter(arr, callback){
+//   let newarr = []
+//    for(let i = 0; i<arr.length; i++){
+
+//      if(callback(arr[i])){
+//        newarr.push(arr[i])
+//      } 
+//    }
+//   return newarr  
+// }
+// // let callback = ()=>{
+  
+// // }
+// let res = myFilter(arr, (ele)=> ele == "suraj")
+
+// console.log(res)
+
+
+
+// let db = ["js","react","nodejs","mongodb"]
+
+// let keyword = "js"
+
+// let rs = db.filter((ele)=>{
+//  return ele == keyword
+// })
+
+// console.log(rs)
+
+let arr =  [1,2,3,4]
+
+
+// let res = arr.reduce((accum,current, index)=>{
+//   return index % 2 != 0 ? accum + current :""
+// }, 0)
+
+
+// console.log(res)
+// let res = arr.map((ele) => ele ** 5)
+// bag += ele + "-"
+// console.log(res)
+
+// index == arr.length -1 ? bag += ele : bag += ele + "-"
+
+let arr1 = [
+  {"name":"suraj", "age":24,"profile":"dev"},
+  {"name":"js", "age":30, "profile":"desiner"},
+  {"name":"repl", "age":25,"profile":'dev'},
+  {"name":"react", "age":26},
+  {"name":"mongo", "age":20},
+]
+
+// let res = arr1.filter((ele)=>{
+// // return ele.profile == "dev" ? ele.name : ""
+// })
+
+console.log(res)
+
+
+
+
+
+
+
+
+
